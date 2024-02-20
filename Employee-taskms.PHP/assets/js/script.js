72215adf22ab94a42d8af1c74f6d1dec4337f4ca
Gokul31122001 <88575956+Gokul31122001@users.noreply.github.com>
2024-02-20 01:55:00 -0800

    Add files via upload

---
+++ Employee-taskms.PHP/assets/js/script.js
@@ -0,0 +1,30 @@
+<?php
+require 'authentication.php'; // admin authentication check 
+
+// auth check
+if(isset($_SESSION['admin_id'])){
+  $user_id = $_SESSION['admin_id'];
+  $user_name = $_SESSION['admin_name'];
+  $security_key = $_SESSION['security_key'];
+  if ($user_id != NULL && $security_key != NULL) {
+    header('Location: task-info.php');
+  }
+}
+
+if(isset($_POST['login_btn'])){
+ $info = $obj_admin->admin_login_check($_POST);
+}
+
+$page_name="Login";
+include("include/login_header.php");
+
+?>
+
+
+
+
+<?php
+
+include("include/footer.php");
+
+?>
